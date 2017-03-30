10ffbff576fc4570be0060e0484d67f80f71ae8e
VipiUpgrade <lordlider1@gmail.com>
2017-03-30 11:06:11 +0400

    coommite

---
+++ ExWin/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ExWin
+{
+    public partial class Form1 : Form
+    {
+
+
+        Game game = new Game(1);
+
+        new void Update()
+        {
+            label1.Text = game.GetDesc();
+            label1.MaximumSize = new Size(800,400);
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+
+            Button[] btns = new Button[] { button1, button2, button3, button4 };
+            for (int i = 0; i < btns.Length; i++)
+                btns[i].Visible = false;
+
+            string[] acts = game.GetActions();
+            for (int i = 0; i < acts.Length && i < 4; i++)
+            {
+                btns[i].Visible = true;
+                btns[i].Text = acts[i];
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Update();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            game.DoAction(0);
+            Update();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            game.DoAction(1);
+            Update();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            game.DoAction(2);
+            Update();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            game.DoAction(3);
+            Update();
+        }
+
+       
+    }
+}
---
+++ ExWin/Game.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Odbc;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+
+namespace ExWin
+{
+    class Game
+    {
+        int loc_id = 1;
+
+        OleDbConnection GetConn()
+        {
+            string s = ConfigurationManager.AppSettings["conn"];
+            return new OleDbConnection(s);
+        }
+
+        public Game(int startloc)
+        {
+            loc_id = startloc;
+        }
+
+        public string GetDesc()
+        {
+            using (var conn = GetConn())
+            {
+                conn.Open();
+                OleDbCommand cmd = new OleDbCommand("select desc from location where id = " + loc_id, conn);
+                return (string)cmd.ExecuteScalar();
+            }
+        }
+
+        public string[] GetActions()
+        {
+            using (var conn = GetConn())
+            {
+                conn.Open();
+
+                OleDbCommand cmd = new OleDbCommand("select desc from transition where local_id = " + loc_id, conn);
+                DataTable table = new DataTable();
+                table.Load(cmd.ExecuteReader());
+
+                List<string> list = new List<string>();
+                for (int i = 0; i < table.Rows.Count; i++)
+                    list.Add((string)table.Rows[i][0]);
+
+                return list.ToArray();
+            }
+        }
+
+        public void DoAction(int num)
+        {
+            using (var conn = GetConn())
+            {
+                conn.Open();
+
+                OleDbCommand cmd = new OleDbCommand("select target_id from transition where local_id = " + loc_id, conn);
+
+                DataTable table = new DataTable();
+                table.Load(cmd.ExecuteReader());
+
+                loc_id = (int)table.Rows[num][0];
+            }
+
+        }
+    }
+}
